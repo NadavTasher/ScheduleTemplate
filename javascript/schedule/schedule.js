fc6d6841d0e74edfff38633a04890a1b8bf58dbb
Nadav Tasher <tashernadav@gmail.com>
2019-04-29 23:50:47 +0300

    Things, mostly, things.

--- javascript/schedule/schedule.js
+++ javascript/schedule/schedule.js
@@ -4,15 +4,54 @@ function schedule() {
 
 function displaySchedule(schedule) {
     let slotArray = [];
-    for (let time in schedule) {
-        if (schedule.hasOwnProperty(time)) {
-            let name = schedule[time];
-            if (name.length === 0) {
+    for (let key in schedule) {
+        if (schedule.hasOwnProperty(key)) {
+            let value = schedule[key];
+            let minute = parseInt(key, 10);
+            let slot = document.createElement("div");
+            let time = document.createElement("p");
+            slot.scheduleIndex = minute;
+            time.innerText = (minute - minute % 60) / 60 + ":" + (minute % 60 < 10 ? "0" : "") + minute % 60;
+            slot.appendChild(time);
+            if (value === null) {
+                let button = document.createElement("button");
+                button.innerText = "Register";
+                button.onclick = () => {
+                    fill(minute);
+                };
+                slot.appendChild(button);
+            } else {
+                let name = document.createElement("p");
+                name.innerText = value;
+                slot.appendChild(name);
             }
-            console.log(name);
-
+            slotArray.push(slot);
         }
     }
+    slotArray = bubble(slotArray);
+    console.log(slotArray);
+    clear("schedule");
+    for (let s = 0; s < slotArray.length; s++) {
+        get("schedule").appendChild(slotArray[s]);
+    }
+}
+
+function bubble(slotArray) {
+    let changes = 0;
+    for (let i = 0; i < slotArray.length; i++) {
+        if (i < slotArray.length - 1) {
+            if (slotArray[i].hasOwnProperty("scheduleIndex") && slotArray[i + 1].hasOwnProperty("scheduleIndex")) {
+                if (slotArray[i].scheduleIndex > slotArray[i + 1].scheduleIndex) {
+                    let temporary = slotArray[i];
+                    slotArray[i] = slotArray[i + 1];
+                    slotArray[i + 1] = temporary;
+                    changes++;
+                }
+            }
+        }
+    }
+    if (changes > 0) slotArray = bubble(slotArray);
+    return slotArray;
 }
 
 function read(callback) {
@@ -28,7 +67,7 @@ function read(callback) {
                 if (json.read.hasOwnProperty("success")) {
                     if (json.read.success) {
                         if (json.read.hasOwnProperty("list")) {
-                            callback(json.list);
+                            callback(json.read.list);
                         }
                     }
                 }
